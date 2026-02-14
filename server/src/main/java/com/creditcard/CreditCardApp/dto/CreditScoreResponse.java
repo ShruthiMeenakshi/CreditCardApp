d290ecd7ecf9a9d956b0d40ea93ecfc611c8267a
ShruthiMeenakshi <shruthikumar542@gmail.com>
2026-02-14 22:20:28 +0530

    Updated codes of controller and Response

--- server/src/main/java/com/creditcard/CreditCardApp/dto/CreditScoreResponse.java
+++ server/src/main/java/com/creditcard/CreditCardApp/dto/CreditScoreResponse.java
@@ -2,7 +2,6 @@ package com.creditcard.CreditCardApp.dto;
 
 import java.time.LocalDate;
 
-public record CreditScoreResponse(String applicantName, LocalDate dateOfBirth, String panNumber, Double annualIncome) {
+public record CreditScoreResponse(String panNumber, int creditScore) {}
 
-}
 
