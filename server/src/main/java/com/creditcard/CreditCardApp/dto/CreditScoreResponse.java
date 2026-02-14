7559858a5fb59665c12807c2934291936f1d0855
ShruthiMeenakshi <shruthikumar542@gmail.com>
2026-02-14 19:40:05 +0530

    updated code

--- server/src/main/java/com/creditcard/CreditCardApp/dto/CreditScoreResponse.java
+++ server/src/main/java/com/creditcard/CreditCardApp/dto/CreditScoreResponse.java
@@ -1,20 +1,6 @@
 package com.creditcard.CreditCardApp.dto;
 
-public class CreditScoreResponse {
+public record CreditScoreResponse(String panNumber, int creditScore) {
 
-    private String panNumber;
-    private int creditScore;
-
-    public CreditScoreResponse(String panNumber, int creditScore) {
-        this.panNumber = panNumber;
-        this.creditScore = creditScore;
-    }
-
-    public String getPanNumber() {
-        return panNumber;
-    }
-
-    public int getCreditScore() {
-        return creditScore;
-    }
 }
+
