10b4ba2837b3609cb5ad808296ecde115cbc2237
ShruthiMeenakshi <shruthikumar542@gmail.com>
2026-02-14 19:45:19 +0530

    Updated code inApplication after CreditScoreResponse

--- server/src/main/java/com/creditcard/CreditCardApp/model/Application.java
+++ server/src/main/java/com/creditcard/CreditCardApp/model/Application.java
@@ -3,12 +3,15 @@ package com.creditcard.CreditCardApp.model;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Setter
+@Getter
 @Document(collection = "applications")
 public class Application {
-
     @Id
     private String applicationId;
 
@@ -29,77 +32,4 @@ public class Application {
     private int creditLimit;     // ✅ int
     private String rejectionReason;
 
-    // ---------- SETTERS ----------
-    public void setApplicationId(String applicationId) {
-        this.applicationId = applicationId;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public void setPanNumber(String panNumber) {
-        this.panNumber = panNumber;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public void setAnnualIncome(double annualIncome) {
-        this.annualIncome = annualIncome;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public void setCreditScore(int creditScore) {
-        this.creditScore = creditScore;
-    }
-
-    public void setCreditLimit(int creditLimit) {
-        this.creditLimit = creditLimit;
-    }
-
-    public void setRejectionReason(String rejectionReason) {
-        this.rejectionReason = rejectionReason;
-    }
-
-    // ---------- GETTERS ----------
-    public String getApplicationId() {
-        return applicationId;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getPanNumber() {
-        return panNumber;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public double getAnnualIncome() {
-        return annualIncome;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public int getCreditScore() {
-        return creditScore;
-    }
-
-    public int getCreditLimit() {
-        return creditLimit;
-    }
-
-    public String getRejectionReason() {
-        return rejectionReason;
-    }
 }
