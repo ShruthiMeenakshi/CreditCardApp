c6b5e2053fbf28fc558aa758ddeca002c8ee89db
ShruthiMeenakshi <shruthikumar542@gmail.com>
2026-02-20 12:37:16 +0530

    Updated UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import ApplicationStatus from "./components/ApplicationStatus";
 
 function App() {
   return (
-    <div style={{ padding: "30px" }}>
-      <h1>Credit Card Application System</h1>
+    <div className="app-shell">
+      <h1 style={{ textAlign: 'center', marginBottom: 18 }}>Credit Card Application System</h1>
       <ApplicationForm />
-      <hr />
+      <div style={{ width: '100%', maxWidth: 720, margin: '20px 0' }}>
+        <hr />
+      </div>
       <ApplicationStatus />
     </div>
   );
